d19552c6c0f9b7b2dff08355356b67930c0e38ab
Alexander Manzi <amanzi@CNYCN-MacBookPro-005.fios-router.home>
2025-02-21 14:49:03 -0500

    Pre color bar push

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-import styles from "./page.module.css";
 import Landing from "./ui/landing";
 import { options } from "./api/auth/[...nextauth]/options";
 import { getServerSession } from "next-auth";
--- app/ui/landing.tsx
+++ app/ui/landing.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import React, { useState, useEffect } from "react"
-import styled from "styled-components"
+import styled, { css } from "styled-components"
 import LoginDropdown from "./loginDropdown";
 
 const LandingWrapper = styled.div`
@@ -28,9 +28,20 @@ const CircleButton = styled.button`
 
     &:hover {
         background-color: #2549FF;
-        box-shadow: 0px 0px 5px 5px green;
+        box-shadow: 0px 0px 5px 5px #1987FB;
     }
 `;
+
+const ColorBar = styled.div`
+  height: 100px;
+  width: 600px;
+  display: flex;
+`;
+
+const BarSegment = styled.div`
+  width: 20%;
+`;
+
 
 interface LandingProps {
     active: boolean;
@@ -39,21 +50,33 @@ interface LandingProps {
 }
 
 export default function Landing({ active, show, user }: LandingProps ) {
-  const [loggedIn, setLoggedIn] = useState(false);
+  const [logIn, setLogIn] = useState(false);
   const [clicked, setClicked] = useState(false);
 
-  console.log(user)
-
   const handleClick = () => {
-    setLoggedIn(true);
-    setClicked(true);
+    if (user) {
+        setLogIn(false)
+        setClicked(true);
+    } else {
+        setLogIn(true)
+        setClicked(false)
+    }
   };
+
+  const colors = [
+    'rgba(255, 0, 0, 1)',    // Red
+    'rgba(0, 255, 0, 1)',    // Green
+    'rgba(0, 0, 255, 1)',    // Blue
+    'rgba(255, 255, 0, 1)',  // Yellow
+    'rgba(0, 255, 255, 1)',  // Cyan
+    'rgba(255, 0, 255, 1)'   // Magenta
+  ];
 
   return (
     <>
-    {loggedIn && <LoginDropdown active={clicked} />}
+    {logIn && <LoginDropdown active={clicked} />}
     <LandingWrapper>
-      <CircleButton onClick={handleClick}>Turn On</CircleButton>
+        <CircleButton onClick={handleClick}>Haiku Me</CircleButton>
     </LandingWrapper>
     </>
   );
